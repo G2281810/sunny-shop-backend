55bc9da46d0131addb65612b581950f8544d9602
Gustavo Angel <al221810743@gmail.com>
2022-08-09 21:28:49 -0500

    initial commit

---
+++ src/config.schema.ts
@@ -0,0 +1,8 @@
+import * as Joi from '@hapi/joi';
+
+export const configValidationSchema = Joi.object({
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().default(3306).required(),
+  DB_USERNAME: Joi.string().required(),
+  DB_DATABASE: Joi.string().required(),
+});
